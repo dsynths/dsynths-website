fcb34adafdec4854533ef324766de7190f7e7912
Paaji <89406655+crypto-paaji@users.noreply.github.com>
2021-09-05 19:41:56 +0530

    feat: migrate to nextjs

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,39 +1,41 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import { DesktopNavbar } from './Navigation/Desktop'
-import { MobileNavbar } from './Navigation/Mobile'
-import { Sidebar } from './Navigation/Sidebar'
-import { Footer } from './Footer'
+import { DesktopNavbar } from "./Navigation/Desktop";
+import { MobileNavbar } from "./Navigation/Mobile";
+import { Sidebar } from "./Navigation/Sidebar";
+import { Footer } from "./Footer";
 
-import { useWindowSize } from '../../hooks/useWindowSize'
+import { useWindowSize } from "../../hooks/useWindowSize";
 
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Content = styled.div`
   flex: 1;
   overflow: auto;
-`
+`;
 
-export const Layout = ({ children }) => {
-  const size = useWindowSize()
-  const [toggled, setToggled] = useState(false)
+const Layout = ({ children }) => {
+  const size = useWindowSize();
+  const [toggled, setToggled] = useState(false);
 
   const handleToggled = (state) => {
-    setToggled(state)
-  }
+    setToggled(state);
+  };
 
   return (
     <Wrapper>
-      {size.width > 600 && <DesktopNavbar handleToggled={handleToggled}/>}
-      {size.width <= 600 && <MobileNavbar handleToggled={handleToggled}/>}
-      <Content isDesktop={size.width > 600}>{children}</Content >
+      {size.width > 600 && <DesktopNavbar handleToggled={handleToggled} />}
+      {size.width <= 600 && <MobileNavbar handleToggled={handleToggled} />}
+      <Content isDesktop={size.width > 600}>{children}</Content>
       <Footer />
-      <Sidebar toggled={toggled} handleToggled={handleToggled}/>
+      <Sidebar toggled={toggled} handleToggled={handleToggled} />
     </Wrapper>
-  )
-}
+  );
+};
+
+export default Layout;
---
+++ src/pages/_document.js
@@ -0,0 +1,72 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, maximum-scale=1"
+            />
+            <meta name="theme-color" content="#191E38" />
+            <meta
+              name="description"
+              content="Trade stocks, commodities and crypto in a decentralized fashion using DEUS Finance its smart contract technology. No KYC - No Signup. Simply connect your wallet and start trading on multiple chains."
+            />
+
+            <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+            <link
+              rel="apple-touch-icon"
+              sizes="192x192"
+              href="/images/192x192_App_Icon.png"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="512x512"
+              href="/images/512x512_App_Icon.png"
+            />
+
+            <link rel="manifest" href="/manifest.json" />
+            <title>dSynths</title>
+          </>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
