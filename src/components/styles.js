7fd32129b3a6540d1ac0bf5dbaa19c3db46b3f2b
ironcap-crypto <81312657+ironcap-crypto@users.noreply.github.com>
2021-09-09 18:28:40 +0530

    feat: product page basic layout
    Basic layout and styles for product/* pages

--- src/components/styles.js
+++ src/components/styles.js
@@ -15,3 +15,9 @@ export const link = classnames('no-underline hover:text-ds-purple focus:text-ds-
 export const menuLink = classnames(link, 'flex')
 
 export const footerLink = classnames('no-underline hover:text-white focus:text-white')
+
+export const productTitle = classnames(
+  'title-gradient text-transparent bg-clip-text font-bold text-4xl'
+)
+
+export const heroContainer = classnames('h-full flex justify-around items-center hero-gradient')
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       },
       colors: {
         'ds-purple': '#6D44FF',
+        'ds-black': '#34363E',
       },
       boxShadow: {
         nav: '0px 1px 0px #E0E0E0',
